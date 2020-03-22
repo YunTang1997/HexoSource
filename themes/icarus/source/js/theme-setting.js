ec4bc280da7d6f46cfdc7fd2b0326c72d2b06cca
YHCClin <lyhdxzb@icloud.com>
2020-03-22 19:47:21 +0800

    修复夜间模式自动切换

--- themes/icarus/source/js/theme-setting.js
+++ themes/icarus/source/js/theme-setting.js
@@ -19,15 +19,14 @@ var nightIcon;
 
 function applyNight(value) {
     if (value == 'true') {
-        document.body.className += ' night'
-        document.cookie = "isnight=1;usr=guest";
+        document.body.className += ' night';
         if (nightIcon) {
-            nightIcon.className = nightIcon.className.replace(/ fa-moon/g, '') + ' fa-lightbulb'
+            nightIcon.className = nightIcon.className.replace(/ fa-moon/g, '') + ' fa-lightbulb';
         }
     } else {
-        document.body.className = document.body.className.replace(/ night/g, '')
+        document.body.className = document.body.className.replace(/ night/g, '');
         if (nightIcon) {
-            nightIcon.className = nightIcon.className.replace(/ fa-lightbulb/g, '') + ' fa-moon'
+            nightIcon.className = nightIcon.className.replace(/ fa-lightbulb/g, '') + ' fa-moon';
         }
     }
 }
@@ -40,9 +39,9 @@ function findNightIcon() {
     } else {
         nightNav.addEventListener('click', switchNight);
         if (isNight) {
-            nightIcon.className = nightIcon.className.replace(/ fa-moon/g, '') + ' fa-lightbulb'
+            nightIcon.className = nightIcon.className.replace(/ fa-moon/g, '') + ' fa-lightbulb';
         } else {
-            nightIcon.className = nightIcon.className.replace(/ fa-lightbulb/g, '') + ' fa-moon'
+            nightIcon.className = nightIcon.className.replace(/ fa-lightbulb/g, '') + ' fa-moon';
         }
     }
 }
